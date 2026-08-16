85a29756025c5305c127423b072e14e8d8c18583
Suhrid Mahmood <mahmoodsuhrid@gmail.com>
2026-08-16 14:35:29 +0800

    initial app structure and front end setup

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'cits3200-3200_15',
-  webDir: 'dist'
-};
-
-export default config;
--- skeleton-app/android/app/src/main/java/com/cits3200/osteoplot/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.cits3200.osteoplot;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
