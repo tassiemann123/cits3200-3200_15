08155d7bf6288994d748362239bc079b8bbbdd92
Ruan van Zyl <rvanz11p@gmail.com>
2026-08-26 10:35:16 +0800

    Modifications to skeleton-data.ts: Spent the last few days researching into skeleton dimensions and calculating our skeletons coordinates for the set origin at the sacral promont. The coordinates have been updated and the files comments modified for the decided upon origin location. also spotted an error in the bone names and fixed it.

--- sharedbackend_code/skeleton-data.ts
+++ sharedbackend_code/skeleton-data.ts
@@ -32,47 +32,47 @@ export interface BoneDefinition {
 // Constants
 // The default joints structure sets values for the joints to present a default joined skeleton
 
-// NOTE FOR SYSTEM COORDINATES: In the app we are working with an individual skeleton so we zero its coordinates starting from the manubrium and use units of millimetres,
+// NOTE FOR SYSTEM COORDINATES: In the app we are working with an individual skeleton so we zero its coordinates starting from the sacral promont and use units of millimetres,
 // When it comes to the desktop app we will then use a spatial coordinate system where the grave is set and the centre point is zeroed, so all
 // skeletons added to the grave will need to be placed in reference to the spatial zero location via an additional reference vector (some 
 // distance and angle from the spatial zero) such that the skeletons dont all attempt to overlap on the spatial zero when we import their coordinates.
 export const DEFAULT_JOINTS: Record<JointName, Vec3> = {
     // Head and Neck joints
-    head_proximal: {x: 0, y: 0, z: 0},
-    centre_of_head: {x: 0, y: 0, z: 0},
-    chin: {x: 0, y: 0, z: 0},
-    manubrium: {x: 0, y: 0, z: 0},
+    head_proximal: {x: 0, y: 820, z: 70},
+    centre_of_head: {x: 0, y: 703, z: 70},
+    chin: {x: 0, y: 620, z: 150},
+    manubrium: {x: 0, y: 509, z: 180},
 
-    // Spine base
+    // Spine base our set origin point for a skeletons coordinates
     sacral_promontory: {x: 0, y: 0, z: 0},
 
     // Left arm
-    left_shoulder: {x: 0, y: 0, z: 0},
-    left_elbow: {x: 0, y: 0, z: 0},
-    left_wrist: {x: 0, y: 0, z: 0},
-    left_fingertips: {x: 0, y: 0, z: 0},
+    left_shoulder: {x: 249, y: 529, z: 70},
+    left_elbow: {x: 281, y: 205, z: 70},
+    left_wrist: {x: 292, y: -57, z: 70},
+    left_fingertips: {x: 292, y: -259, z: 70},
 
     // Right arm
-    right_shoulder: {x: 0, y: 0, z: 0},
-    right_elbow: {x: 0, y: 0, z: 0},
-    right_wrist: {x: 0, y: 0, z: 0},
-    right_fingertips: {x: 0, y: 0, z: 0},
+    right_shoulder: {x: -249, y: 529, z: 70},
+    right_elbow: {x: -281, y: 205, z: 70},
+    right_wrist: {x: -292, y: -57, z: 70},
+    right_fingertips: {x: -292, y: -259, z: 70},
 
     // Left pelvis and leg
-    left_ilium_superior: {x: 0, y: 0, z: 0},
-    left_acetabulum: {x: 0, y: 0, z: 0},
-    left_ischium: {x: 0, y: 0, z: 0},
-    left_knee: {x: 0, y: 0, z: 0},
-    left_ankle: {x: 0, y: 0, z: 0}, 
-    left_toes: {x: 0, y: 0, z: 0},
+    left_ilium_superior: {x: 190, y: 41, z: 30},
+    left_acetabulum: {x: 180, y: -50, z: 70},
+    left_ischium: {x: 90, y: -80, z: 0},
+    left_knee: {x: 129, y: -459, z: 90},
+    left_ankle: {x: 69, y: -861, z: 60}, 
+    left_toes: {x: 69, y: -966, z: 266},
 
     // Right pelvis and leg
-    right_ilium_superior: {x: 0, y: 0, z: 0},
-    right_acetabulum: {x: 0, y: 0, z: 0},
-    right_ischium: {x: 0, y: 0, z: 0},
-    right_knee: {x: 0, y: 0, z: 0},
-    right_ankle: {x: 0, y: 0, z: 0}, 
-    right_toes: {x: 0, y: 0, z: 0}
+    right_ilium_superior: {x: -190, y: 41, z: 30},
+    right_acetabulum: {x: -180, y: -50, z: 70},
+    right_ischium: {x: -90, y: -80, z: 0},
+    right_knee: {x: -129, y: -459, z: 90},
+    right_ankle: {x: -69, y: -861, z: 60}, 
+    right_toes: {x: -69, y: -966, z: 266}
 
 };
 
@@ -87,7 +87,7 @@ export const DEFAULT_BONES: BoneDefinition[] = [
     {name: "spine", startJoint: "manubrium", endJoint: "sacral_promontory"},
 
     // Left arm
-    {name: "left_clavicle ", startJoint: "manubrium", endJoint: "left_shoulder"},
+    {name: "left_clavicle", startJoint: "manubrium", endJoint: "left_shoulder"},
     {name: "left_humerus", startJoint: "left_shoulder", endJoint: "left_elbow"},
     {name: "left_forearm", startJoint: "left_elbow", endJoint: "left_wrist"},
     {name: "left_hand", startJoint: "left_wrist", endJoint: "left_fingertips"},
